641cac408731ccef4792de0b39773929637033e9
Swetha <swethar604@gmail.com>
2026-05-20 13:02:26 +0530

    Added Lateral Surface Area

--- VolumeCuboid.py
+++ VolumeCuboid.py
@@ -1,3 +1,4 @@
 l,b,h=map(int,input("Enter the length, breadth and height of the cuboid: ").split() )
 volume=l*b*h
 print(f"The volume of the cuboid is: {volume}" )
+print(f"LSA of the cuboid is: {2*(l*b + b*h + h*l)}" )
